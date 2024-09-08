79e7ca2fba4798518190550355cb3804a8120d09
boyuia <dodylan190@gmail.com>
2024-09-08 12:22:36 +1000

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+import os
+import sys
+sys.path.insert(0, os.path.obspath('.'))
+
+project = 'exUI'
+copyright = '2024 exUI'
+extentions = []
+
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
